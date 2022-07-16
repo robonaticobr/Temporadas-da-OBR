d6c178a8e4a0418b1d4186e349aa3b70514ea135
Robonáticos OBR <109423571+robonaticobr@users.noreply.github.com>
2022-07-16 17:12:24 -0300

    Create Godao.ino

---
+++ Temporada 2022/Recover-me/Godao.ino
@@ -0,0 +1,18 @@
+ /*
+ * Programação do segue linha Godão v1.0
+ * 
+ * Autor: Colin Lucas Batista Beluco
+ * 
+ * Objetivo: Robô autônomo seguidor de linha e resgate
+ * 
+ * Data de início de prática: YY/08/2021
+ * 
+ * Data da primeira versão - início em: 13/03/2022
+ * 
+ * Colaboração: Napão; Luís Antônio Silva; Rayssa Besse, Nicolas Richard e Arthur Orosco (robô de treino desenvolvido em 2017); Aser Gomes;
+ */
+
+
+String ORGULHO_DE_SER_ROBONATICO = "    7565    ";
+String NOS_DIVERTIMOS = "Comemoramos os erros e os acertos";
+String Erros = "Existe para ser acertado";
